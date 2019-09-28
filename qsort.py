ef2e6b0fefef39e057ce8d47a374577f87d1377c
leoparadis777 <leopennylove@gmail.com>
2019-09-28 16:42:13 -0700

    qsort.py report.txt fix

--- qsort.py
+++ qsort.py
@@ -9,3 +9,21 @@ def sort(a):
         right = [x for x in a[1:] if x >= pivot]
         return [sort(left)] + [pivot] + [sort(right)]
 
+def sorted(t):
+    td = sort(t)
+    return td[::2]
+    print (td[::2])
+
+def search(t,x):
+    tt = sorted(t)
+    x = input()
+    if x in tt:
+        print ("Ture")
+    else:
+        print ("Fals")
+
+def insert(t,x):
+    ti = sorted(t)
+    x = input()
+    ti.insert(x)
+    return sort(t)
