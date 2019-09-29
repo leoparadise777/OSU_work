b67921f61f7ab56669a6abbe49ebb10c0b62ac12
leoparadis777 <leopennylove@gmail.com>
2019-09-28 17:45:07 -0700

    qselect first try

---
+++ qselect.py
@@ -0,0 +1,18 @@
+def sort(k,a):
+    # the base condition
+    if a == []:
+        return []
+    # sort part
+    else:
+        k = input()
+        pivot = random.sample(a,1)
+        left = [x for x in a if x < pivot]
+        right = [x for x in a[1:] if x >= pivot]
+        if k == len(left) + 1:
+            return pivot
+        elif k > len(left) + 1:
+            k = k-1
+            return sort(k,left)
+        else
+            k = k-1
+            return sort(k,right)
--- qsort.py
+++ qsort.py
@@ -9,21 +9,27 @@ def sort(a):
         right = [x for x in a[1:] if x >= pivot]
         return [sort(left)] + [pivot] + [sort(right)]
 
+
 def sorted(t):
     td = sort(t)
     return td[::2]
-    print (td[::2])
+    print(td[::2])
+
 
 def search(t,x):
     tt = sorted(t)
     x = input()
     if x in tt:
-        print ("Ture")
+        print("Ture")
     else:
-        print ("Fals")
+        print("Fals")
+
 
 def insert(t,x):
     ti = sorted(t)
     x = input()
-    ti.insert(x)
-    return sort(t)
+    if (x for x in ti):
+        return sort(ti)
+    else:
+        ti.insert(x)
+        return sort(ti)
