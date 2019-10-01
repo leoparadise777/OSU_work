1181ca1888dacb7fe413be7584da3cdfd9639099
leoparadis777 <leopennylove@gmail.com>
2019-09-30 18:52:39 -0700

    qselect random

--- qselect.py
+++ qselect.py
@@ -2,7 +2,7 @@ from random import randint
 
 def qselect(i, a):
     if len(a) > 0:
-        # i = randint(0, len(a)-1)
+        i = randint(0, len(a)-1)
         a[0], a[i] = a[i], a[0]
         pivot = a[0]
         left = [x for x in a if x < pivot]
@@ -11,9 +11,9 @@ def qselect(i, a):
         if i == len(left) + 1:
             return pivot
         elif i > len(left) + 1:
-            return qselect(i-1, right)
+            return qselect(i-len(left)+1, right)
         else:
-            return qselect(i-1, left)
-    #return a
+            return qselect(i, left)
+
 
 #print(qselect(4 ,[3,7,6,9,1,8,10,4]))
